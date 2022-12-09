dc263920ef26b1a0725cbd0d8f5951b90a982f1a
pradeepkmr22 <57754125+pradeepkmr22@users.noreply.github.com>
2022-12-10 01:12:21 +0530

    updated

--- test.tf
+++ test.tf
@@ -1,12 +1,12 @@
 name "pradeep" {
-  address     = "git::https://github.name.gamil.com/drive.google/app/?ref=10"
+  address     = "git::https://github.paddu0.gamil.com/drive.google/app/?ref=10"
   mobile  = "git::https://github.8149785451.gamil.com/drive.google/app/?ref=10"
 }
 name "mounica" {
-  address     = "git::https://github.name.gamil.com/drive.google/app/?ref=10"
+  address     = "git::https://github.paddu0.gamil.com/drive.google/app/?ref=10"
   mobile  = "git::https://github.9963515.gamil.com/drive.google/app/?ref=10"
 }
 name "krishna" {
-  address     = "git::https://github.name.gamil.com/drive.google/app/?ref=10"
+  address     = "git::https://github.paddu0.gamil.com/drive.google/app/?ref=10"
   mobile  = "git::https://github.91771151515.gamil.com/drive.google/app/?ref=10"
 }
